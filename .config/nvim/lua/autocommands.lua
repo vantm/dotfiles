23ddfeb2f5e275a884ffe315264ef141368e449d
Van Tran <tmvan.1903@gmail.com>
2023-08-27 12:19:39 +0700

    remove trailing on save

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,13 +1,13 @@
 vim.cmd [[highlight NormalFloat ctermbg=DarkBlue ctermfg=DarkGray]]
 vim.cmd [[highlight Pmenu ctermbg=DarkBlue ctermbg=LightGray]]
 
-local my_auto_commands = vim.api.nvim_create_augroup("MyAutoCommands", {
+local my_augroup = vim.api.nvim_create_augroup("MyAutoGroup", {
     clear = false
 })
 
 vim.api.nvim_create_autocmd({ "InsertEnter" }, {
     pattern = { "*.*" },
-    group = my_auto_commands,
+    group = my_augroup,
     callback = function()
         vim.opt.relativenumber = false
     end
@@ -15,8 +15,14 @@ vim.api.nvim_create_autocmd({ "InsertEnter" }, {
 
 vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
     pattern = { "*" },
-    group = my_auto_commands,
+    group = my_augroup,
     callback = function()
         vim.opt.relativenumber = true
     end
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*" },
+    group = my_augroup,
+    command = [[%s/\s\+$//ge]]
 })
