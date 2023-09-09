193e0727b476456e4e7129245c83f8afe3233d20
Van Tran <tmvan.1903@gmail.com>
2023-09-09 18:00:57 +0700

    clean code autocommands

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,13 +1,13 @@
 vim.cmd [[highlight NormalFloat ctermbg=DarkBlue ctermfg=DarkGray]]
 vim.cmd [[highlight Pmenu ctermbg=DarkBlue ctermbg=LightGray]]
 
-local my_augroup = vim.api.nvim_create_augroup("MyAutoGroup", {
+local augroup = vim.api.nvim_create_augroup("augroup_mine", {
     clear = false
 })
 
 vim.api.nvim_create_autocmd({ "InsertEnter" }, {
     pattern = { "*.*" },
-    group = my_augroup,
+    group = augroup,
     callback = function()
         vim.opt.relativenumber = false
     end
@@ -15,7 +15,7 @@ vim.api.nvim_create_autocmd({ "InsertEnter" }, {
 
 vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
     pattern = { "*" },
-    group = my_augroup,
+    group = augroup,
     callback = function()
         vim.opt.relativenumber = true
     end
@@ -23,6 +23,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     pattern = { "*" },
-    group = my_augroup,
+    group = augroup,
     command = [[%s/\s\+$//ge]]
 })
