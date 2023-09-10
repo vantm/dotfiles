03beb3dfe67a131d424636d913a7d23bf1bc0506
Van Tran <tmvan.1903@gmail.com>
2023-09-10 10:59:03 -0700

    Make nvim tree float

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -4,6 +4,15 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup {
     view = {
         number = true,
-        relativenumber = true
+        relativenumber = true,
+        float = {
+            enable = true,
+            open_win_config = {
+                width = 80,
+                height = 40,
+                row = 1,
+                col = 1,
+            },
+        }
     }
 }
