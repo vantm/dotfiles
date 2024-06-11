35c9a81a19679acedf08ba2b15b44ea9bf547ea7
Van Tran <tmvan.1903@gmail.com>
2024-06-11 22:21:05 +0700

    add update script

---
+++ update.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+cp ~/.zprofile .zprofile
+cp ~/.zshrc .zshrc
+cp ~/.ideavimrc .zshrc
+cp ~/.tmux.conf .tmux.conf
+
+mkdir -p .config/mpd
+cp ~/.config/mpd/mpd.conf .config/mpd
+
+cp -r ~/.config/hypr .config
+cp -r ~/.config/kitty .config
+cp -r ~/.config/lazygit .config
+cp -r ~/.config/nvim .config
+cp -r ~/.config/rofi .config
+cp -r ~/.config/swayidle .config
+cp -r ~/.config/swaylock .config
+cp -r ~/.config/waybar .config
