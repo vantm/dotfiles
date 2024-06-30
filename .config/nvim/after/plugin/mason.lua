6c3464abf8ff2fb2f7c4e4dbdd85770037f8be2c
Van Tran <tmvan.1903@gmail.com>
2024-06-30 20:36:33 +0700

    remove unnecesssary mason preinstall languages

--- .config/nvim/after/plugin/mason.lua
+++ .config/nvim/after/plugin/mason.lua
@@ -4,18 +4,11 @@ require 'mason-lspconfig'.setup {
     ensure_installed = {
         'bashls',
         'cssls',
-        'gopls',
         'html',
         'jsonls',
-        'jdtls',
-        'lua_ls',
-        'pyright',
-        'rust_analyzer',
-        'tsserver',
-        'omnisharp',
         'yamlls'
     },
-    automatic_installation = true
+    automatic_installation = false
 }
 
 require 'null-ls'.setup {}
