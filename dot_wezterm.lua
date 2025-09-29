b1eba412b94fb7247cfeb1dd19bc8cc567fa65b1
Van Tran <tmvan.1903@gmail.com>
2025-09-29 13:14:06 +0700

    feat: remove modal.wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "GruvboxDarkHard"
 
 config.font = wezterm.font("FantasqueSansM Nerd Font")
-config.font_size = 12
+config.font_size = 11
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
@@ -27,9 +27,5 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 
 	config.default_prog = pwsh
 end
-
-local modal = wezterm.plugin.require("https://github.com/MLFlexer/modal.wezterm")
-modal.apply_to_config(config)
-modal.set_default_keys(config)
 
 return config
