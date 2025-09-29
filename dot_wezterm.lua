ed86075718f2f49ff10459fd9a82cad90c803354
Van Tran <tmvan.1903@gmail.com>
2025-09-29 12:42:18 +0700

    feat: add wezterm

---
+++ dot_wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "GruvboxDarkHard"
+
+config.font = wezterm.font("FantasqueSansM Nerd Font")
+config.font_size = 12
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_max_width = 40
+
+config.launch_menu = config.launch_menu or {}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	local pwsh = { os.getenv("USERPROFILE") .. "\\scoop\\apps\\pwsh\\current\\pwsh.exe", "-NoLogo" }
+
+	table.insert(config.launch_menu, {
+		label = "PowerShell Core",
+		args = pwsh,
+	})
+
+	table.insert(config.launch_menu, {
+		label = "PowerShellCore",
+		args = { "powershell.exe", "-NoLogo" },
+	})
+
+	config.default_prog = pwsh
+end
+
+local modal = wezterm.plugin.require("https://github.com/MLFlexer/modal.wezterm")
+modal.apply_to_config(config)
+modal.set_default_keys(config)
+
+return config
