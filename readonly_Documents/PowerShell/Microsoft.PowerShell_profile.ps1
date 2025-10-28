5c969238fd88ce6dc0ebadd5c86ed83590af847e
Van Tran <tmvan.1903@gmail.com>
2025-10-28 12:23:34 +0700

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -168,4 +168,6 @@ If ("$(where.exe /Q kaf && echo 1)" -ne "") {
     Invoke-Expression (@(kaf completion powershell) -replace " ''\)$"," ' ')" -join "`n")
 }
 
+Invoke-Expression (&{ direnv hook pwsh | Out-String })
+
 Import-Module -ErrorAction Ignore "$PSScriptRoot/PrivateFunctions.ps1"
